c27122c2233831b4098ca64b1fa3d960dafcefe0
hyeonsikyee <112372096+hyeonsikyee@users.noreply.github.com>
2022-10-11 17:52:41 +0900

    Create app.js

---
+++ app.js
@@ -0,0 +1,149 @@
+var express  = require('express');
+
+var app      = express();
+
+var http     = require('http').Server(app);
+
+var io       = require('socket.io')(http);
+
+ 
+
+const path = require('path');
+
+ 
+
+var SerialPort = require('serialport').SerialPort;
+
+var ReadlineParser = require('@serialport/parser-readline').ReadlineParser;
+
+var parsers    = SerialPort.parsers;
+
+var sp = new SerialPort( {
+
+  path:'COM6',
+
+  baudRate: 9600
+
+});
+
+var parser     = sp.pipe(new ReadlineParser({
+
+  delimiter: '\r\n'
+
+}));
+
+var port = 3000;
+
+ 
+
+sp.pipe(parser);
+
+ 
+
+sp.on('open', () => console.log('Port open'));
+
+ 
+
+parser.on('data', function(data))
+
+{
+
+	console.log(data.toString());
+
+	if(data.substring(0,5) == "bucket"){
+
+		if(data.substring(3,4) == "1")	bucketStatus = "on";
+
+		else							bucketStatus = "off";
+
+		io.emit('bucket', bucketStatus);
+
+		console.log('bucket status: ' + bucketStatus);
+
+	}
+
+	else if(data.substring(0,3) == "cow"){
+
+		if(data.substring(3,4) == "1")	bucketStatus = "on";
+
+		else							bucketStatus = "off";
+
+		io.emit('cow', bucketStatus);
+		console.log('bucket status: ' + bucketStatus);
+
+	}
+	
+	else if(data.substring(0,2) <= "70"){
+		
+		data2 = data.substring(0,2)
+
+		bucketStatus = "on";
+
+	else	bucketStatus = "off";
+
+		io.emit(data2, bucketStatus);
+		console.log('bucket status: ' + bucketStatus);
+
+	}
+
+});
+
+ 
+
+app.get('/open',function(req,res)
+
+{
+
+	sp.write('led1\n\r', function(err){
+
+		if (err) {
+
+            return console.log('Error on write: ', err.message);
+
+        }
+
+        console.log('send: bucket open');
+
+		res.writeHead(200, {'Content-Type': 'text/plain'});
+
+		//res.end('bucket open\n');
+
+	});
+
+});
+
+ 
+
+app.get('/close',function(req,res)
+
+{
+
+	sp.write('led0\n\r', function(err){
+
+		if (err) {
+
+            return console.log('Error on write: ', err.message);
+
+        }
+
+        console.log('send: bucket close');
+
+		res.writeHead(200, {'Content-Type': 'text/plain'});
+
+		//res.end('bucket close\n');
+
+	}); 
+
+});
+
+ 
+
+app.use(express.static(__dirname + '/public'));
+
+ 
+
+http.listen(port, function() {  // server.listen(port, function() {
+
+    console.log('listening on *:' + port);
+
+});
