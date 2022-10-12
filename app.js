a28c0b58957842acb6ce73f0d34fc367780287c8
seongyun325 <115126000+seongyun325@users.noreply.github.com>
2022-10-12 10:50:10 +0900

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var app      = express();
 var http     = require('http').Server(app);
 
 var io       = require('socket.io')(http);
-
+var tmp;
  
 
 const path = require('path');
@@ -20,7 +20,7 @@ var parsers    = SerialPort.parsers;
 
 var sp = new SerialPort( {
 
-  path:'COM6',
+  path:'/dev/ttyUSB0',
 
   baudRate: 9600
 
@@ -28,7 +28,7 @@ var sp = new SerialPort( {
 
 var parser     = sp.pipe(new ReadlineParser({
 
-  delimiter: '\r\n'
+  delimiter: '\n'
 
 }));
 
@@ -44,59 +44,52 @@ sp.on('open', () => console.log('Port open'));
 
  
 
-parser.on('data', function(data))
+parser.on('data', function(data)
 
 {
 
-	console.log(data.toString());
+    console.log(data.toString());
+	if (data.substring(0, 3) == "dis") {
+		val = data.substring(3);
+		distance = parseInt(val);
+		console.log('distance: ' + distance);
+     	if (distance < 70) {
+         // 1. child-process모듈의 spawn 취득
+         const spawn = require('child_process').spawn;
 
-	if(data.substring(0,5) == "bucket"){
+         // 2. spawn을 통해 "python 파이썬파일.py" 명령어 실행
+         const result = spawn('python3', ['yolo_uvc2.py']);
 
-		if(data.substring(3,4) == "1")	bucketStatus = "on";
+       
+         // 3. stdout의 'data'이벤트리스너로 실행결과를 받는다.
+         result.stdout.on('data', function(data) {
+            console.log(data.toString());
+            if (data == "cow") {
+            	sp.write('servo1\n\r', function(err){
+				  if (err) {
+						return console.log('Error on write: ', err.message);
+				  }
+				  console.log('send: bucket open');
+				  res.writeHead(200, {'Content-Type': 'text/plain'});
+			   }
+            }
+         });
 
-		else							bucketStatus = "off";
-
-		io.emit('bucket', bucketStatus);
-
-		console.log('bucket status: ' + bucketStatus);
-
-	}
-
-	else if(data.substring(0,3) == "cow"){
-
-		if(data.substring(3,4) == "1")	bucketStatus = "on";
-
-		else							bucketStatus = "off";
-
-		io.emit('cow', bucketStatus);
-		console.log('bucket status: ' + bucketStatus);
-
-	}
-	
-	else if(data.substring(0,2) <= "70"){
-		
-		data2 = data.substring(0,2)
-
-		bucketStatus = "on";
-
-	else	bucketStatus = "off";
-
-		io.emit(data2, bucketStatus);
-		console.log('bucket status: ' + bucketStatus);
-
-	}
-
-});
-
- 
-
+         // 4. 에러 발생 시, stderr의 'data'이벤트리스너로 실행결과를 받는다.
+         result.stderr.on('data', function(data) {
+            console.log(data.toString());
+         });
+        }
+     }
+      
+      
 app.get('/open',function(req,res)
 
 {
 
-	sp.write('led1\n\r', function(err){
+   sp.write('servo1\n\r', function(err){
 
-		if (err) {
+      if (err) {
 
             return console.log('Error on write: ', err.message);
 
@@ -104,13 +97,20 @@ app.get('/open',function(req,res)
 
         console.log('send: bucket open');
 
-		res.writeHead(200, {'Content-Type': 'text/plain'});
+      res.writeHead(200, {'Content-Type': 'text/plain'});
 
-		//res.end('bucket open\n');
+      //res.end('bucket open\n')led1
+;
 
-	});
+   });
 
 });
+      //io.emit('bucket', bucketStatus);
+
+});
+
+ 
+
 
  
 
@@ -118,9 +118,9 @@ app.get('/close',function(req,res)
 
 {
 
-	sp.write('led0\n\r', function(err){
+   sp.write('servo0\n\r', function(err){
 
-		if (err) {
+      if (err) {
 
             return console.log('Error on write: ', err.message);
 
@@ -128,22 +128,22 @@ app.get('/close',function(req,res)
 
         console.log('send: bucket close');
 
-		res.writeHead(200, {'Content-Type': 'text/plain'});
+      res.writeHead(200, {'Content-Type': 'text/plain'});
 
-		//res.end('bucket close\n');
+      //res.end('bucket close\n');
 
-	}); 
+   }); 
 
 });
-
  
 
 app.use(express.static(__dirname + '/public'));
 
  
-
 http.listen(port, function() {  // server.listen(port, function() {
 
     console.log('listening on *:' + port);
 
 });
+
+
